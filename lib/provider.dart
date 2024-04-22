bd2309ff21eeb04944d8dd268a8cbaddcc294037
Sourav Raj <souravrajsrk11@gmail.com>
2024-04-22 09:23:07 +0530

    update

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class RandomNumberProvider extends ChangeNotifier {
   bool _isLoading = false;
-
   bool get isLoading => _isLoading;
 
   void setLoading(bool loading) {
